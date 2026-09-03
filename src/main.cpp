2c522345a4d97b19cbc55c27a9811aa611ad5132
adilson113 <adilsondaslives@gmail.com>
2026-09-02 23:41:08 -0300

    Refactor mod information and remove logger usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <mod/amlmod.h>
-#include <mod/logger.h>
 
 MYMOD(
-    com.prf.multas,
-    PRF Multas,
+    com.adilson.prfmultas,
+    PRFMultas,
     1.0,
     Adilson
 )
@@ -12,14 +11,8 @@ NEEDGAME(com.rockstargames.gtasa)
 
 ON_MOD_PRELOAD()
 {
-    logger->SetTag("PRF Multas");
 }
 
 ON_MOD_LOAD()
 {
-    logger->Info("================================");
-    logger->Info("PRF MULTAS MOD CARREGADO!");
-    logger->Info("GTA San Andreas detectado");
-    logger->Info("Sistema iniciado com sucesso");
-    logger->Info("================================");
 }
