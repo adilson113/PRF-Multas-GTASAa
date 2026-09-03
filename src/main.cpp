eb1db57ab0f855d249fab1017a1cf000f5627e68
adilson113 <adilsondaslives@gmail.com>
2026-09-02 23:07:59 -0300

    Add PRFMultasInit function for logging initialization

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <android/log.h>
+
+#define LOG_TAG "PRFMultas"
+
+#define LOGI(...) __android_log_print(
+    ANDROID_LOG_INFO,
+    LOG_TAG,
+    __VA_ARGS__
+)
+
+extern "C"
+void PRFMultasInit()
+{
+    LOGI("PRF Multas Mod iniciado!");
+}
